d0b6e1ddfb49ae37b2b9a2e6e76a8ada91cf5411
hj <20125182@hanmail.net>
2021-05-08 16:24:35 +0900

    Component, JSX 이해

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+function Food() {
+  return (
+    <h1>I love potato</h1>
+  )
+}
+
+
+function App() {
+  return (
+    <div>
+      <h1>HELLO!!!</h1>
+      <Food />
+    </div>
+  ); 
+}
+
+export default App;
