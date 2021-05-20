5a303994e80cdf0b265f7fc612c98783d3989f55
hj <20125182@hanmail.net>
2021-05-20 16:51:34 +0900

    planning the movie app

--- src/App.js
+++ src/App.js
@@ -2,47 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
+class App extends React.Component {
+  state = {
+    isLoading : true,
+    movies: []
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+  }
 
-const foodILike = [
-  {
-    id: 1,
-    name : "kimchi",
-    rating: 5,
-  },
-  {
-    id: 2,
-    name : "kimbab",
-    rating: 3,
-  },
-];
-
-function Food({ name, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating} / 5.0</h4>
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-}
-
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food 
-          name = {dish.name}
-          key = {dish.id} 
-          rating = {dish.rating} 
-        />
-      ))}
-    </div>
-  ); 
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <div>
+        {isLoading ? "Loading..." : "We are Ready"}
+      </div>
+    )}
 }
 
 export default App;
